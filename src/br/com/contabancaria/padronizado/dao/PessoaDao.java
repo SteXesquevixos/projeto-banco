40b35e75605ba4609f362fa9df38f3601be91c0d
Stephany Xesquevixos <stexesquevixos@gmail.com>
2022-08-16 19:26:02 -0300

    Fix

--- src/br/com/contabancaria/padronizado/dao/PessoaDao.java
+++ src/br/com/contabancaria/padronizado/dao/PessoaDao.java
@@ -2,16 +2,12 @@ package br.com.contabancaria.padronizado.dao;
 
 import br.com.contabancaria.padronizado.dao.model.Pessoa;
 
-import java.sql.Date;
 import java.util.List;
 
-public interface PessoaDao {
+public interface PessoaDAO {
 
     List<Pessoa> buscarPessoas();
 
     Pessoa buscarPessoaPorId(Long id);
 
-    Pessoa buscarPessoaPorNome(String nome);
-
-    Pessoa buscarPessoaPorDataNascimento(Date data_nascimento);
 }

871257656f1a8c98557f6f21d038d6ee0da99131
Wanderson Xesquevixos de Siqueira <wanderson.siqueira@mandae.com.br>
2022-08-16 20:18:07 -0300

    Novos métodos filtros pessoa

--- src/br/com/contabancaria/padronizado/Main.java
+++ src/br/com/contabancaria/padronizado/Main.java
@@ -4,8 +4,7 @@ import br.com.contabancaria.padronizado.dao.PessoaDao;
 import br.com.contabancaria.padronizado.dao.impl.PessoaDaoImpl;
 import br.com.contabancaria.padronizado.dao.model.Pessoa;
 
-import java.sql.*;
-import java.time.LocalDate;
+import java.sql.Date;
 import java.util.List;
 
 public class Main {
@@ -19,51 +18,51 @@ public class Main {
         principal.listagemPessoas(pessoaDao);
 
         principal.filtrarPessoaPorId(pessoaDao);
+
         principal.filtrarPessoaPorNome(pessoaDao);
-        principal.filtrarPessoaPorDataNascimento(pessoaDao);
+
+        principal.filtrarPessoaDataNascimento(pessoaDao);
 
     }
 
-    public void filtrarPessoaPorId(PessoaDao pessoaDAO) {
+
+    public void filtrarPessoaPorId(PessoaDao pessoaDao) {
         Long id = 3L;
-        Pessoa pessoa = pessoaDAO.buscarPessoaPorId(id);
+        Pessoa pessoa = pessoaDao.buscarPessoaPorId(id);
 
         System.out.println(" -------- UNICA PESSOA ----- ");
         if(pessoa != null) {
-            System.out.println( pessoa.getId() + "\t" + pessoa.getNome()  + "\t" +  pessoa.getDataNascimento());
-        }
-    }
-
-    public void filtrarPessoaPorNome(PessoaDao pessoaDao) {
-        String nome = "Stephany";
-        Pessoa pessoa = pessoaDao.buscarPessoaPorNome(nome);
-
-        System.out.println(" -------- UNICA PESSOA ----- ");
-        if(pessoa != null) {
-            System.out.println( pessoa.getId() + "\t" + pessoa.getNome()  + "\t" +  pessoa.getDataNascimento());
-        }
-    }
-
-    public void filtrarPessoaPorDataNascimento(PessoaDao pessoaDao) {
-//        LocalDate data = LocalDate.now();
-//        Date dataSql = java.sql.Date.valueOf(data);
-        Date data_nascimento = Date.valueOf("1977-08-02");
-        Pessoa pessoa = pessoaDao.buscarPessoaPorDataNascimento(data_nascimento);
-
-        System.out.println(" -------- UNICA PESSOA ----- ");
-        if(pessoa != null) {
-            System.out.println( pessoa.getId() + "\t" + pessoa.getNome()  + "\t" +  pessoa.getDataNascimento());
+            System.out.println( pessoa.getId() + " -  " + pessoa.getNome()  +  pessoa.getDataNascimento());
         }
     }
 
     public void listagemPessoas(PessoaDao pessoaDAO) {
-        List<Pessoa> pessoas = pessoaDAO.buscarPessoas();
-        imprimir(pessoas);
+        List<Pessoa> Pessoas = pessoaDAO.buscarPessoas();
+        imprimir(Pessoas);
     }
 
     public static void imprimir(List<Pessoa> pessoas) {
         for (Pessoa pessoa : pessoas) {
-            System.out.println( pessoa.getId() + "\t" +  pessoa.getNome()  + "\t" +  pessoa.getDataNascimento());
+            System.out.println( pessoa.getId() + " -  " +  pessoa.getNome()  +  pessoa.getDataNascimento());
+        }
+    }
+    public void filtrarPessoaPorNome(PessoaDao pessoaDao) {
+        String nome = "Alexander";
+        Pessoa pessoa = pessoaDao.buscarPessoaPorNome(nome);
+
+        System.out.println(" -------- UNICA PESSOA ----- ");
+        if(pessoa != null) {
+            System.out.println( pessoa.getId() + " -  " + pessoa.getNome()  +  pessoa.getDataNascimento());
+        }
+    }
+    public void filtrarPessoaDataNascimento(PessoaDao pessoaDao){
+        Date data_nascimento = Date.valueOf("2009-04-01");
+        Pessoa pessoa = pessoaDao.buscarPessoaPorDataNascimento(data_nascimento);
+
+        System.out.println(" -------- UNICA PESSOA ----- ");
+        if(pessoa != null) {
+            System.out.println( pessoa.getId() + " -  " + pessoa.getNome()  +  pessoa.getDataNascimento());
         }
     }
 }
+
